b5363f33af97f30c984b8fb965b0f73ee8119cf0
= <=>
2023-04-28 13:55:06 -0600

    agregar 'tipoOperacion' a situacion patrimonial

--- src/db/schemas/prestamo.ts
+++ src/db/schemas/prestamo.ts
@@ -1,9 +1,14 @@
 import { DuenoTitularPrestamoSchema } from './dueno_titular_prestamo';
 import { Schema } from 'mongoose';
 import { TipoBienPrestamoSchema } from './tipo_bien_prestamo';
-
+import { addNullValue } from '../../library/utils';
+import { TipoOperacion} from '../../types/enums';
 
 export const PrestamoSchema = new Schema({
+  tipoOperacion: {
+    type: String,
+    enum: addNullValue(TipoOperacion),
+  },
   tipoBien: TipoBienPrestamoSchema,
   duenoTitular: DuenoTitularPrestamoSchema,
 });
