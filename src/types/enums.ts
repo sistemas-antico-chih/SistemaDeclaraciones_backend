4be9375f758c5419ebfc46c0d3f97539513ef3ea
= <=>
2025-07-21 14:39:49 -0600

    aviso

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -63,7 +63,7 @@ export enum TipoDeclaracion {
   INICIAL = 'INICIAL',
   MODIFICACION = 'MODIFICACION',
   CONCLUSION = 'CONCLUSION',
-  AVISO = 'AVISO',
+  AVISO = 'AVISOO',
 }
 
 export enum TipoFideicomiso {
