be83a290c7ebfdd9221d5e9e24cb8311808da825
= <=>
2025-10-08 09:32:12 -0600

    inicio sesion curp o correo

--- src/types/input/users.ts
+++ src/types/input/users.ts
@@ -11,6 +11,7 @@ export interface UserProfileInput {
 }
 
 export interface UserSignUpInput extends UserProfileInput {
-  username: string;
+  //username: string;
+  identifier: string;
   password: string;
 }
